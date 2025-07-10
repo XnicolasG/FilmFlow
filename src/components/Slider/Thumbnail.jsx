ec87cf2bda702e46fa5ea426a897a5a6ac4ba9db
Nicolás García <nicolasg.ngl@gmail.com>
2025-07-09 21:41:17 -0500

    add initial styles to dyamic pages

--- src/components/Slider/Thumbnail.jsx
+++ src/components/Slider/Thumbnail.jsx
@@ -1,14 +1,16 @@
 import { SliderButtons } from "./SliderButtons"
 
 export const Thumbnail = ({ movies, onNext, onPrev }) => {
+    console.log(movies);
+    
     return (
         <section className="thumbnail">
             <SliderButtons onNext={onNext} onPrev={onPrev} />
             {
                 movies.map((movie) => (
-                    <div className={`thumbnail__item`}>
+                    <a href={`/media/${movie.source}/${movie.media_type}/${movie.id}`} className={`thumbnail__item`}>
                         <img src={`https://image.tmdb.org/t/p/w1280${movie.poster_path}`} alt={movie.title} />                    
-                    </div>
+                    </a>
                 ))
             }
         </section>
